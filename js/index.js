c12c3dc421de9405728e9f69e686243670b58ebd
HD_Johnny <johnny.wang@holdendata.com>
2019-11-01 18:20:32 +0800

    11

---
+++ js/index.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+    fnResize();
+    var th_width = $(".news-module li").eq(0).width();
+    var th_left = $(".news-module li").eq(0).offset().left;
+    var slider_width = $(".news-slider").width();
+    var slider_left = th_left + (th_width / 2) - slider_width / 2;
+    $(".news-slider").css("left", slider_left);
+    $(".news-module li").on("click", function () {
+        var n = $(this).index();
+        var th_width = $(this).width();
+        var th_left = $(this).offset().left;
+        var slider_width = $(".news-slider").width();
+        var slider_left = th_left + (th_width / 2) - slider_width / 2;
+        $(".news-slider").css("left", slider_left);
+        $(this).addClass("active").siblings().removeClass("active");
+    });
+
+
+    $(".memberitem").hover(function(){
+        $(this).children("div").toggle();
+    },function(){
+        $(this).children("div").toggle();
+    });
+});
+function mScroll(id) {
+    $("html,body").stop(true);
+    $("html,body").animate({ scrollTop: $("#" + id).offset().top }, 1000);
+    return false;
+}
+function fnResize() {
+    var deviceWidth = document.documentElement.clientWidth || window.innerWidth;
+    if (deviceWidth >= 1920) {
+     deviceWidth = 1920;
+    }
+    if (deviceWidth <= 750) {
+     deviceWidth = 750;
+    }
+    document.documentElement.style.fontSize = deviceWidth / 19.2 + 'px';
+   };
+window.onresize=fnResize();
+
